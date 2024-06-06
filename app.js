dd3de87c65b84b87388a9ffb8a08097161003955
Takeshi <134627945+takeshiridas@users.noreply.github.com>
2024-06-06 09:24:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Função para avançar/navegar pelos slides
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Função para mostrar um slide específico
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
+// Automatizar a transição dos slides
+setInterval(function() {
+    plusSlides(1);
+}, 5000); // Mudar a cada 5 segundos
+
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
+
+
